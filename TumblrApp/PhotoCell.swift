1b85acbea77481e3da22c60a4d2db389ffe128a7
Judy Gatobu <judygatobu@Judys-MacBook-Air.local>
2018-02-15 18:28:33 -0500

    Initial, broken code.

---
+++ TumblrApp/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TumblrApp
+//
+//  Created by Judy Gatobu on 2/15/18.
+//  Copyright © 2018 Judy Gatobu. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
