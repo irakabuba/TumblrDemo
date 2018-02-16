bdb5636f75eb497d2abddd764a5b64ef4eb877d3
Judy Gatobu <judygatobu@Judys-MacBook-Air.local>
2018-02-15 19:01:14 -0500

    Initial, broken code.

--- TumblrApp/DetailViewController.swift
+++ TumblrApp/DetailViewController.swift
@@ -1,35 +1,41 @@
 //
 //  DetailViewController.swift
-//  TumblrApp
+//  TumblrDemoAK
 //
-//  Created by Judy Gatobu on 2/15/18.
-//  Copyright © 2018 Judy Gatobu. All rights reserved.
+//  Created by Judy Gatobu on 2/10/18.
+//  Copyright © 2018 Abubakari Kitwala. All rights reserved.
 //
 
 import UIKit
-
+import AlamofireImage
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var DetailImage: UIImageView!
+    
+    var photoURL: URL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        DetailImage.af_setImage(withURL: photoURL)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
--- TumblrApp/PhotoCell.swift
+++ TumblrApp/PhotoCell.swift
@@ -1,24 +1,26 @@
 //
 //  PhotoCell.swift
-//  TumblrApp
+//  TumblrDemoAK
 //
-//  Created by Judy Gatobu on 2/15/18.
-//  Copyright © 2018 Judy Gatobu. All rights reserved.
+//  Created by Judy Gatobu on 2/8/18.
+//  Copyright © 2018 Abubakari Kitwala. All rights reserved.
 //
 
 import UIKit
 
 class PhotoCell: UITableViewCell {
-
+    
+    @IBOutlet weak var notimageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
